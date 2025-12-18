8eb56c1d5b16840421886252cba572c6dd261df0
bala-amypo <bala@amypo.in>
2025-12-18 14:32:21 +0000

    User commit

--- src/main/java/com/example/demo/repository/VerificationLogRepo.java
+++ src/main/java/com/example/demo/repository/VerificationLogRepo.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.model.FraqudCheckResult;
+import com.example.demo..FraqudCheckResult;
 
 public interface FraudCheckResultRepo extends JpaRepository<FraudCheckResult , Long>{
     
