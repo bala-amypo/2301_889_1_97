787d611f4da89baa123e524404a6cb6fb199f8e8
bala-amypo <bala@amypo.in>
2025-12-18 09:58:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationLog.java
+++ src/main/java/com/example/demo/entity/VerificationLog.java
@@ -1,5 +1,11 @@
 package com.example.demo.entity;
 
 public class VerificationLog {
+    @Id
+    private Long id;
+    private LocalDateTime verifiedAt;
+    private String SUCCESS;
+    private String FAILED;
+    private String ipAddress;
     
 }
