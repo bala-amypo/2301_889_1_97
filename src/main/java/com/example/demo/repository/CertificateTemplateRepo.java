42e61defcb5a05dfbbab4b2301b3c1dd19149485
bala-amypo <bala@amypo.in>
2025-12-18 14:38:21 +0000

    User commit

--- src/main/java/com/example/demo/repository/CertificateTemplateRepo.java
+++ src/main/java/com/example/demo/repository/CertificateTemplateRepo.java
@@ -2,8 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.model.FraqudCheckResult;
+import com.example.demo.entity.CertificateTemplate;
 
-public interface FraudCheckResultRepo extends JpaRepository<FraudCheckResult , Long>{
+public interface CertificateTemplateRepo extends JpaRepository<CertificateTemplate , Long>{
     
 }
