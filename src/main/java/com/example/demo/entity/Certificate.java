73554a2f984d14cb5960419a0e8b397154da3b2c
bala-amypo <bala@amypo.in>
2025-12-18 09:43:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/Certificate.java
+++ src/main/java/com/example/demo/entity/Certificate.java
@@ -3,5 +3,8 @@ package com.example.demo.entity;
 public class Certificate{
     @Id
     private Long id;
-    
+    private LocalDate issuedDate;
+    private String qrCodeUrl;
+    @Column(unique=true)
+    private String verificationCode;
 }
