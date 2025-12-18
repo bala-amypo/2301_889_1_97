8db2284f03e2a322941cc63ceecf95752b8b02d6
bala-amypo <bala@amypo.in>
2025-12-18 09:13:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/CertificateTemplate.java
+++ src/main/java/com/example/demo/entity/CertificateTemplate.java
@@ -2,5 +2,10 @@ package com.example.demo.entity;
 
 public class CertificateTemplate{
     @Id
-    
+    private Long id;
+    @Column(unique=true)
+    private String templateName;
+    private String backgroundUrl;
+    private String fontStyle;
+    private String signatureName;
 }
