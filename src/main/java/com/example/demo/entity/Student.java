925335cb1f2f571feb8e8e13b50fdd0d56e09481
bala-amypo <bala@amypo.in>
2025-12-18 09:08:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -10,4 +10,39 @@ public class Student{
     private String email;
     @Column(unique=true)
     private String rollNumber;
+
+    
+    public Student(Long id, String name, String email, String rollNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.rollNumber = rollNumber;
+    }
+    public Student() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getRollNumber() {
+        return rollNumber;
+    }
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+    
 }
