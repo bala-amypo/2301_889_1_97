579e2117b74700827786b4c149796d61c886508e
bala-amypo <bala@amypo.in>
2025-12-18 14:35:21 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepo.java
+++ src/main/java/com/example/demo/repository/StudentRepo.java
@@ -2,8 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.model.FraqudCheckResult;
+import com.example.demo.entity.Student;
 
-public interface FraudCheckResultRepo extends JpaRepository<FraudCheckResult , Long>{
+public interface StudentRepo extends JpaRepository<Student , Long>{
     
 }
