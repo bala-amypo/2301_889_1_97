4a2563b609f63b5f8a5d5b7d5d3c2e661d6ea1f6
bala-amypo <bala@amypo.in>
2025-12-20 17:03:33 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -1,41 +1,26 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.entity.Student;
-import com.example.demo.repository.StudentRepository;
-import com.example.demo.service.StudentService;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
 @Service
 public class StudentServiceImpl implements StudentService {
 
-    private final StudentRepository studentRepository;
+    private final StudentRepository repo;
 
-    public StudentServiceImpl(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
+    public StudentServiceImpl(StudentRepository repo) {
+        this.repo = repo;
     }
 
-    @Override
     public Student addStudent(Student student) {
-
-        if (studentRepository.findByEmail(student.getEmail()).isPresent()
-                || studentRepository.findByRollNumber(student.getRollNumber()).isPresent()) {
+        if (repo.findByEmail(student.getEmail()).isPresent()
+         || repo.findByRollNumber(student.getRollNumber()).isPresent())
             throw new RuntimeException("Student email exists");
-        }
 
-        return studentRepository.save(student);
+        return repo.save(student);
     }
 
-    @Override
     public List<Student> getAllStudents() {
-        return studentRepository.findAll();
+        return repo.findAll();
     }
 
-    @Override
     public Student findById(Long id) {
-        return studentRepository.findById(id)
-                .orElseThrow(() ->
-                        new RuntimeException("Student not found"));
+        return repo.findById(id)
+            .orElseThrow(() -> new RuntimeException("Student not found"));
     }
 }
--- src/main/java/com/example/demo/service/Impl/TemplateServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TemplateServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service.impl;
+package com.example.demo.service.;
 
 import com.example.demo.entity.CertificateTemplate;
 import com.example.demo.repository.CertificateTemplateRepository;
