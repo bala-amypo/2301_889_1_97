7ab5175c2578048f3668e1117983b5d1460e595a
bala-amypo <bala@amypo.in>
2025-12-20 18:24:34 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -6,15 +6,8 @@ import lombok.*;
 import java.util.List;
 
 @Entity
-@Table(name = "students", 
-       uniqueConstraints = {
-           @UniqueConstraint(columnNames = "email"),
-           @UniqueConstraint(columnNames = "rollNumber")
-       })
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
+
 public class Student {
 
     @Id
@@ -31,5 +24,62 @@ public class Student {
 
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
     private List<Certificate> certificates;
+
+    
+    
+    public Student(Long id, String name, String email, String rollNumber, List<Certificate> certificates) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.rollNumber = rollNumber;
+        this.certificates = certificates;
+    }
+
+    public Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public List<Certificate> getCertificates() {
+        return certificates;
+    }
+
+    public void setCertificates(List<Certificate> certificates) {
+        this.certificates = certificates;
+    }
+
+
+
 }
+
 
