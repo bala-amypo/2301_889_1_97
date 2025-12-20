41023b97d0e02e327980a998007c2ed88e45b1a9
bala-amypo <bala@amypo.in>
2025-12-20 18:21:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,55 +1,35 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.List;
 
 @Entity
-@Table(name = "students",
-uniqueConstraints = {
- @UniqueConstraint(columnNames = "email"),
- @UniqueConstraint(columnNames = "rollNumber")
-})
-
+@Table(name = "students", 
+       uniqueConstraints = {
+           @UniqueConstraint(columnNames = "email"),
+           @UniqueConstraint(columnNames = "rollNumber")
+       })
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false, unique = true)
     private String rollNumber;
 
-    
-    public Student(Long id, String name, String email, String rollNumber) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.rollNumber = rollNumber;
-    }
-    public Student() {
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getRollNumber() {
-        return rollNumber;
-    }
-    public void setRollNumber(String rollNumber) {
-        this.rollNumber = rollNumber;
-    }
-    
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+    private List<Certificate> certificates;
 }
+
--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -1,1 +1,39 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public Student addStudent(Student student) {
+        studentRepository.findByEmail(student.getEmail()).ifPresent(s -> {
+            throw new RuntimeException("Student email exists");
+        });
+        studentRepository.findByRollNumber(student.getRollNumber()).ifPresent(s -> {
+            throw new RuntimeException("Student email exists");
+        });
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student findById(Long id) {
+        return studentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+    }
+}
 
