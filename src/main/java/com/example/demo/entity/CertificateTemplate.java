159059cabe293570f9bb586b298d6510505906aa
bala-amypo <bala@amypo.in>
2025-12-20 17:24:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/CertificateTemplate.java
+++ src/main/java/com/example/demo/entity/CertificateTemplate.java
@@ -3,56 +3,15 @@ package com.example.demo.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 
-public class CertificateTemplate{
-    @Id
+@Entity
+@Table(name="certificate_templates",
+uniqueConstraints=@UniqueConstraint(columnNames="templateName"))
+
+public class CertificateTemplate {
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    @Column(unique=true)
     private String templateName;
-    @URL(message = "Background URL must be a valid URL")
     private String backgroundUrl;
     private String fontStyle;
     private String signatureName;
-
-    
-    public CertificateTemplate(Long id, String templateName,
-            @URL(message = "Background URL must be a valid URL") String backgroundUrl, String fontStyle,
-            String signatureName) {
-        this.id = id;
-        this.templateName = templateName;
-        this.backgroundUrl = backgroundUrl;
-        this.fontStyle = fontStyle;
-        this.signatureName = signatureName;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getTemplateName() {
-        return templateName;
-    }
-    public void setTemplateName(String templateName) {
-        this.templateName = templateName;
-    }
-    public String getBackgroundUrl() {
-        return backgroundUrl;
-    }
-    public void setBackgroundUrl(String backgroundUrl) {
-        this.backgroundUrl = backgroundUrl;
-    }
-    public String getFontStyle() {
-        return fontStyle;
-    }
-    public void setFontStyle(String fontStyle) {
-        this.fontStyle = fontStyle;
-    }
-    public String getSignatureName() {
-        return signatureName;
-    }
-    public void setSignatureName(String signatureName) {
-        this.signatureName = signatureName;
-    }
-    public CertificateTemplate() {
-    }
 }
