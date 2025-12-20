b8868b5cefb2df01bfb9c6cad4f0ae6320f2b0af
bala-amypo <bala@amypo.in>
2025-12-20 18:09:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,11 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 public class User {
 
     @Id
@@ -26,5 +22,57 @@ public class User {
     @Builder.Default
     private String role = "STAFF";
 
-    
+    public User(Long id, String name, String email, String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+
 }
