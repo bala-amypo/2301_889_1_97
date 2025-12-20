6d548513db2340bfebb3fbf53c73f2d05fecabbb
bala-amypo <bala@amypo.in>
2025-12-20 15:34:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationLog.java
+++ src/main/java/com/example/demo/entity/VerificationLog.java
@@ -2,10 +2,59 @@ package com.example.demo.entity;
 
 public class VerificationLog {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "certificate_id")
+    private Certificate certificate;
+
     private LocalDateTime verifiedAt;
-    private String SUCCESS;
-    private String FAILED;
+
+    private String status;
     private String ipAddress;
+    
+    
+    public VerificationLog(Long id, Certificate certificate, LocalDateTime verifiedAt, String status,
+            String ipAddress) {
+        this.id = id;
+        this.certificate = certificate;
+        this.verifiedAt = verifiedAt;
+        this.status = status;
+        this.ipAddress = ipAddress;
+    }
+    public VerificationLog() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Certificate getCertificate() {
+        return certificate;
+    }
+    public void setCertificate(Certificate certificate) {
+        this.certificate = certificate;
+    }
+    public LocalDateTime getVerifiedAt() {
+        return verifiedAt;
+    }
+    public void setVerifiedAt(LocalDateTime verifiedAt) {
+        this.verifiedAt = verifiedAt;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getIpAddress() {
+        return ipAddress;
+    }
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
     
 }
