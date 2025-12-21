aa5cd99b56c775557dd806cd141e7b3d69f7a5d2
bala-amypo <bala@amypo.in>
2025-12-21 07:12:37 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -4,9 +4,8 @@ package com.example.demo.controller;
 import com.example.demo.entity.User;
 
 import com.example.demo.service.UserService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
