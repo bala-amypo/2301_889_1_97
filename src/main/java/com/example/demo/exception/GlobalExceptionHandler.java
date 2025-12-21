99ba5fdaf913989403e8cb6d2eac65001789b642
bala-amypo <bala@amypo.in>
2025-12-21 13:17:14 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,4 +1,5 @@
-package com.example.handling.exception;
+package com.example.demo.exception;  
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,24 +9,19 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-@ExceptionHandler(ResourceNotFoundException.class)
-public ResponseEntity<String>
-handleNotFound(ResourceNotFoundException ex) {
-return new ResponseEntity<>(ex.getMessage(),
-HttpStatus.NOT_FOUND);
-}
-@ExceptionHandler(MethodArgumentNotValidException.class)
-public ResponseEntity<Map<String, String>>
-handleValidationErrors(
-MethodArgumentNotValidException ex) {
-Map<String, String> errors = new HashMap<>();
-ex.getBindingResult().getFieldErrors().forEach(error ->
-errors.put(error.getField(),
-error.getDefaultMessage())
-);
-return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-}
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors()
+            .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
