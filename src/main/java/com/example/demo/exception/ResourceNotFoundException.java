fb6ae8dafaababde1a454b5c3e4fd06331a00aad
bala-amypo <bala@amypo.in>
2025-12-21 12:20:14 +0000

    User commit

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,5 +1,7 @@
 package com.example.demo.exception;
 
+import com.example.demo.exception.ResourceNotFoundException;
+
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
