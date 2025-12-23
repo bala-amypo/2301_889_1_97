ae6f80583d031d176d19f3c233e2bc7710f5a314
bala-amypo <bala@amypo.in>
2025-12-23 04:55:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -49,7 +49,7 @@ public class User {
 
     public String getName() { 
         return name; 
-        }
+    }
     public void setName(String name) { this.name = name; }
 
     public String getEmail() { return email; }
