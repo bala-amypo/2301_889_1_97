35f28a5f8e01848095f400f1da7a29f39c2bb453
bala-amypo <bala@amypo.in>
2025-12-25 09:51:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/CertificateTemplate.java
+++ src/main/java/com/example/demo/entity/CertificateTemplate.java
@@ -1,55 +1,99 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
 
 @Entity
-@Table(
-    name = "certificate_templates",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = "templateName")
-    }
-)
+@Table(name = "certificate_templates")
+
+@Builder
 public class CertificateTemplate {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String templateName;
 
     private String backgroundUrl;
+
     private String fontStyle;
+
     private String signatureName;
 
-    public CertificateTemplate() {}
-
-    public CertificateTemplate(Long id, String templateName, String backgroundUrl,
-                               String fontStyle, String signatureName) {
+    @OneToMany(mappedBy = "template")
+    private List<Certificate> certificates;
+    
+    
+    public CertificateTemplate(Long id, String templateName, String backgroundUrl, String fontStyle,
+            String signatureName, List<Certificate> certificates) {
         this.id = id;
         this.templateName = templateName;
         this.backgroundUrl = backgroundUrl;
         this.fontStyle = fontStyle;
         this.signatureName = signatureName;
+        this.certificates = certificates;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getTemplateName() { return templateName; }
-    public void setTemplateName(String templateName) { this.templateName = templateName; }
-
-    public String getBackgroundUrl() { 
-        return backgroundUrl; 
-    }
-    public void setBackgroundUrl(String backgroundUrl) { 
-        this.backgroundUrl = backgroundUrl; 
+    public CertificateTemplate() {
     }
 
-    public String getFontStyle() { return fontStyle; }
-    public void setFontStyle(String fontStyle) { this.fontStyle = fontStyle; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getSignatureName() { return signatureName; }
-    public void setSignatureName(String signatureName) { this.signatureName = signatureName; }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public String getBackgroundUrl() {
+        return backgroundUrl;
+    }
+
+    public void setBackgroundUrl(String backgroundUrl) {
+        this.backgroundUrl = backgroundUrl;
+    }
+
+    public String getFontStyle() {
+        return fontStyle;
+    }
+
+    public void setFontStyle(String fontStyle) {
+        this.fontStyle = fontStyle;
+    }
+
+    public String getSignatureName() {
+        return signatureName;
+    }
+
+    public void setSignatureName(String signatureName) {
+        this.signatureName = signatureName;
+    }
+
+    public List<Certificate> getCertificates() {
+        return certificates;
+    }
+
+    public void setCertificates(List<Certificate> certificates) {
+        this.certificates = certificates;
+    }
+
+
+
+
+
 }
+
+
 
--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,15 +1,13 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
 
 @Entity
-@Table(
-    name = "students",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = "email"),
-        @UniqueConstraint(columnNames = "rollNumber")
-    }
-)
+@Table(name = "students")
+@Builder
 public class Student {
 
     @Id
@@ -18,33 +16,69 @@ public class Student {
 
     private String name;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String email;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String rollNumber;
 
-    public Student() {}
+    @OneToMany(mappedBy = "student")
+    private List<Certificate> certificates;
 
-    public Student(Long id, String name, String email, String rollNumber) {
+    
+    
+    public Student(Long id, String name, String email, String rollNumber, List<Certificate> certificates) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.rollNumber = rollNumber;
+        this.certificates = certificates;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Student() {
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getRollNumber() { return rollNumber; }
-    public void setRollNumber(String rollNumber) { this.rollNumber = rollNumber; }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public List<Certificate> getCertificates() {
+        return certificates;
+    }
+
+    public void setCertificates(List<Certificate> certificates) {
+        this.certificates = certificates;
+    }
+
+
+
+
 }
-
-
-
