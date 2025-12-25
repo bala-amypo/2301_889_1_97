58d109272393900323fbe66c999ee0773241b68e
bala-amypo <bala@amypo.in>
2025-12-25 12:42:30 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-	 exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-    }
-)
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -15,3 +11,6 @@ public class DemoApplication {
 	}
 
 }
+
+
+ithu enoda portal project demo application
