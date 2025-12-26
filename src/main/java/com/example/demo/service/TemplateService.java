aac2e44f86d81d3d1a9c31d93190c7cd16fa32d9
bala-amypo <bala@amypo.in>
2025-12-26 15:21:12 +0000

    User commit

--- src/main/java/com/example/demo/service/TemplateService.java
+++ src/main/java/com/example/demo/service/TemplateService.java
@@ -1,15 +1,9 @@
 package com.example.demo.service;
-
 import com.example.demo.entity.CertificateTemplate;
-
 import java.util.List;
 
 public interface TemplateService {
-
     CertificateTemplate addTemplate(CertificateTemplate template);
-
     List<CertificateTemplate> getAllTemplates();
-
     CertificateTemplate findById(Long id);
 }
-
